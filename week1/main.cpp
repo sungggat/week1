9ba8b868f52897d8760b8d5024c9afab1de0daf0
sunggataiymbay <sunggat.aiymbay@icloud.com>
2018-06-06 14:12:11 +0300

    Initial Commit

---
+++ week1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  week1
+//
+//  Created by Sunggat Aiymbay on 06.06.2018.
+//  Copyright © 2018 Sunggat Aiymbay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
